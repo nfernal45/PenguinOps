f3cd99064da8324378c6d301054d2820cbcc0eea
gavrilov <gavrilov@lanit.ru>
2024-10-30 23:33:47 +0400

    Прекращение выполнения скрипта при нехватке места на диске во втором скрипте + небольшой рефакторинг

--- shell/download_backup/scripts/temp.sh
+++ shell/download_backup/scripts/temp.sh
@@ -4,37 +4,49 @@ set -euxo pipefail
 
 REMOTE_IP_ADDR="192.168.88.15"
 SSH_LOGIN="user"
-REMOTE_SOURCE_DIR="backups/nfs"
-BACKUP_DEST="backup/nfs"
+REMOTE_SOURCE_DIR="/backups/nfs"
+BACKUP_DEST="/backup/nfs"
 BACKUP_RETENTION_DAYS=30
 ARCHIVE_NAME="nfs"
+BACKUP_FILE="$ARCHIVE_NAME"-"$CURRENT_DATE"
+BACKUP_FILE_PATH="$BACKUP_DEST"/"$BACKUP_FILE"
 CURRENT_DATE=$(date +"%Y-%m-%d")
+DISK_USAGE_THRESHOLD=85
 
-mkdir -p "/$BACKUP_DEST"
+E_NOTFREESPACE=47 # Недостаточно свободного места для бэкапа
+E_NOTCREATEDIR=25
+
+mkdir -p "$BACKUP_DEST" || exit $E_NOTCREATEDIR
+
+# Проверка дискового пространства
+DISK_USAGE=$(df / | grep $BACKUP_DEST | awk '{ print $5 }' | sed 's/%//g')
+if [ ${DISK_USAGE} -gt ${DISK_USAGE_THRESHOLD} ]; then
+    exit $E_NOTFREESPACE
+fi
 
 # Проверяем успешность создания архива
-if scp -r "$SSH_LOGIN"@"$REMOTE_IP_ADDR":/"$REMOTE_SOURCE_DIR"/"$ARCHIVE_NAME"-"$CURRENT_DATE" /"$BACKUP_DEST"/; then
+if scp -r "$SSH_LOGIN"@"$REMOTE_IP_ADDR":"$REMOTE_SOURCE_DIR"/"$BACKUP_FILE" /"$BACKUP_DEST"/; then
 
     # Исправляем путь запуска скрипта в файле service
-    sed -i "s!/$REMOTE_SOURCE_DIR/!/$BACKUP_DEST/!" /"$BACKUP_DEST"/"$ARCHIVE_NAME-$CURRENT_DATE"/"$ARCHIVE_NAME-$CURRENT_DATE".md5
+    sed -i "s!$REMOTE_SOURCE_DIR/!/$BACKUP_DEST/!" "$BACKUP_FILE_PATH"/"$BACKUP_FILE".md5
 
     # Проверяем существование файла для проверки целостности
-    if [ -e /"$BACKUP_DEST"/"$ARCHIVE_NAME"-"$CURRENT_DATE"/"$ARCHIVE_NAME-$CURRENT_DATE".md5 ]; then     
+    if [ -e "$BACKUP_DEST"/"$BACKUP_FILE"/"$BACKUP_FILE".md5 ]; then     
         
 
         # Проверяем целостность архива
-        if md5sum --quiet -c /"$BACKUP_DEST"/"$ARCHIVE_NAME"-"$CURRENT_DATE"/"$ARCHIVE_NAME-$CURRENT_DATE".md5; then       
-            echo "$CURRENT_DATE arhive Check OK!" >> /$BACKUP_DEST/$ARCHIVE_NAME-"$CURRENT_DATE"/check.log
+        if md5sum --quiet -c /"$BACKUP_DEST"/"$BACKUP_FILE"/"$BACKUP_FILE".md5; then       
+            echo "$CURRENT_DATE arhive Check OK!" >> $BACKUP_DEST/$ARCHIVE_NAME-"$CURRENT_DATE"/check.log
 
             # Удаляем старые бэкапы по дате    
             DATE_BEFORE=$(date -d "-$BACKUP_RETENTION_DAYS days" +"%Y-%m-%d")
-            find /"$BACKUP_DEST" -type d -name "$ARCHIVE_NAME-$DATE_BEFORE" -exec rm -r {} \;
+            find "$BACKUP_DEST" -type d -name "$ARCHIVE_NAME-$DATE_BEFORE" -exec rm -r {} \;
         else
-            echo "$CURRENT_DATE arhive Check is NOT OK!" >> /"$BACKUP_DEST"/"$ARCHIVE_NAME-$CURRENT_DATE"/check.log
+            echo "$CURRENT_DATE arhive Check is NOT OK!" >> "$BACKUP_FILE_PATH"/check.log
             exit 1
         fi
     else
-        echo "$CURRENT_DATE check file not found" >> /"$BACKUP_DEST"/"$ARCHIVE_NAME-$CURRENT_DATE"/check.log
+        echo "$CURRENT_DATE check file not found" >> "$BACKUP_FILE_PATH"/check.log
     fi
 else
     echo "Произошла ошибка при скачивании архива $CURRENT_DATE"
