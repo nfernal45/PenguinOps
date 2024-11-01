0df910da1591605e857643c15d548d24c72f252b
gavrilov <gavrilov@lanit.ru>
2024-11-02 00:10:37 +0400

    fix paths

--- shell/create_backup/scripts/temp.sh
+++ shell/create_backup/scripts/temp.sh
@@ -22,17 +22,18 @@ create_backup()
 {
     for DIR in ${TARGET_DIRS}; do
 
-        [[ ! -d "${DIR}" ]] || exit $E_NOTDIR
-            
-        TARGET_DIR="${DIR##*/}"
-        BACKUP_FULL_PATH="${BACKUP_DEST}/${TARGET_DIR}-${CURRENT_DATE}"
-        ARCHIVE_NAME_FULL="${BACKUP_DEST}/${TARGET_DIR}-${CURRENT_DATE}.tar.gz"
-        PURGE_DATE=$(date -d "-${BACKUP_RETENTION_DAYS} days" +"%Y-%m-%d")
+        [[ ! -d "${DIR}" ]] && exit $E_NOTDIR
 
-        mkdir -p "${BACKUP_FULL_PATH}/logs"
-        tar czfv "$ARCHIVE_NAME_FULL ${DIR}" > "$BACKUP_FULL_PATH/logs/verbose.log" && \
-        md5sum "$ARCHIVE_NAME_FULL" > "$BACKUP_FULL_PATH.md5"  
-        find "$BACKUP_DEST" -type d -name "$ARCHIVE_NAME-$PURGE_DATE" -exec rm -rf {} \;
+        TARGET_DIR="${DIR##*/}"
+        BACKUP_DIR="${BACKUP_DEST}/${TARGET_DIR}-${CURRENT_DATE}"
+        ARCHIVE_PATH="${BACKUP_DIR}/${TARGET_DIR}-${CURRENT_DATE}.tar.gz"
+        LOG_PATH="${BACKUP_DIR}/logs/verbose.log"
+        MD5_PATH="${BACKUP_DIR}/${TARGET_DIR}-${CURRENT_DATE}.md5"
+
+        mkdir -p "${BACKUP_DIR}/logs"
+        tar -v -czf "${ARCHIVE_PATH}" "${DIR}" > "${LOG_PATH}" && \
+            md5sum "${ARCHIVE_PATH}" > "${MD5_PATH}"
+        find "${BACKUP_DEST}" -type d -name "${TARGET_DIR}-${PURGE_DATE}" -exec rm -rf {} \;
     done
 }
 
